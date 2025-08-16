716e4f3bd0e8c21a990d85027e10d378fc16a320
mdulas1 <maawiyajibrintanimu126@gmail.com>
2025-08-16 17:23:17 +0100

    my c programs

---
+++ calculate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char operator;
+    double num1, num2, result;
+
+    printf("enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+}
---
+++ switch.c
@@ -0,0 +1,25 @@
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+                break;
+            case '-':
+                result = num1 - num2;
+                printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+                break;
+            case '*':
+                result = num1 * num2;
+                printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+                break;
+            case '/':
+                if (num2 != 0) { // Handle division by zero
+                    result = num1 / num2;
+                    printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+                } else {
+                    printf("Error: Division by zero is not allowed.\n");
+                }
+                break;
+            default:
+                printf("Error: Invalid operator.\n");
+        }
